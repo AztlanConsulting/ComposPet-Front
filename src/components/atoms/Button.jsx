577207ccef9066c54c7f9f1c74e4d29c9329a7de
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2026-03-17 23:51:54 -0600

    [ADD] colos and first button atom

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../../css/button.css";
+
+// sizes: ["mini", "small", "medium", "large", "xl"]
+// type: ["info", "cancel", "accept", "login", "warning"]
+
+export default function Button({
+    size = "medium",
+    type = "accept",
+    className = "",
+    onClick = () => {},
+    children,
+}) {
+    return (
+        <button className={`button ${size} ${type} ${className}`} onClick={onClick}>
+            {children}
+        </button>
+    );
+}
