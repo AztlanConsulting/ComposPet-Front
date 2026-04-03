094955b69abb9f56141ef3795cc563be68c03143
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2026-04-03 15:16:54 -0600

    [AGREGAR] Comentarios de acuerdo al estandar

--- src/components/organisms/SignIn.jsx
+++ src/components/organisms/SignIn.jsx
@@ -1,7 +1,22 @@
 import InputComponent from "../molecules/InputComponent";
 import Button from "../atoms/Button";
-
 import React from 'react'
+
+/**
+ * Formulario de inicio de sesión con campos de correo y contraseña.
+ * Delega el manejo del estado y las validaciones al componente padre;
+ * este componente es exclusivamente de presentación.
+ * Los errores de validación se reflejan visualmente aplicando la clase
+ * `input-error` al campo correspondiente.
+ *
+ * @param {Function} [onEmailChange=()=>{}] - Manejador del evento de cambio en el campo de correo.
+ * @param {Function} [onPasswordChange=()=>{}] - Manejador del evento de cambio en el campo de contraseña.
+ * @param {string} [emailError=""] - Mensaje de error del correo. Si no está vacío, aplica estilo de error al input.
+ * @param {string} [passwordError=""] - Mensaje de error de la contraseña. Si no está vacío, aplica estilo de error al input.
+ * @returns {JSX.Element} Formulario con inputs de correo, contraseña y botón de inicio de sesión.
+ * @see InputComponent
+ * @see Button
+ */
 
 export default function SignIn({
     onEmailChange = () => {},
