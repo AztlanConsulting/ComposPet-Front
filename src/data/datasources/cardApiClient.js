fa25ab8b15cb4f40572a6ce432738b47788fde12
Yessica <a01273574@tec.mx>
2026-04-19 22:19:34 -0600

    [AGREGAR] apiClient, repositorio y repositorio de interfaz para tarjetas

---
+++ src/data/datasources/cardApiClient.js
@@ -0,0 +1,30 @@
+import api from '../../api/axiosConfig';
+import { handleHttpError } from '../infrastructure/httpErrorHandler';
+
+/**
+ * Cliente HTTP para el módulo de tarjetas del cliente de ComposPet.
+ * Gestiona la obtención y actualización de formularios semanales.
+ */
+
+export class CardApiClient {
+    /**
+     * Obtiene la solicitud de recolección actual del cliente para el rango semanal indicado.
+     * Si no existe, el servidor genera una nueva automáticamente.
+     * * @async
+     * @param {string} clientId - Id único del cliente.
+     * @returns {Promise<Object>} Datos de la solicitud de recolección.
+     * @throws {Error} Si el token es inválido o hay errores de red.
+     */
+
+    async getCardBalance(clientId){
+        try {
+            const response = await api.post('/card/obtener-id-cliente', {
+                clientId
+            });
+
+            return response.data;
+        } catch (error) {
+            handleHttpError(error);
+        }
+    }
+}
---
+++ src/data/repositories/cardRepository.js
@@ -0,0 +1,47 @@
+import {CardIRepository} from '../../domain/repositories/cardInterfaceRepository'
+import {Card} from '../..domain/entities/client';
+
+/**
+ * Implementación concreta del repositorio de tarjetas.
+ * Actúa como adaptador entre el caso de uso y el cliente HTTP,
+ * transformando la respuesta de la API en una entidad `Card` del dominio.
+ *
+ * @extends CardIRepository
+ * @see CardApiClient
+ * @see Card
+ */
+
+export class CardRepository extends CardIRepository {
+    /**
+     * Crea una instancia del repositorio de tarjetas.
+     *
+     * @param {import('../datasources/cardApiClient').CardApiClient} apiClient - Cliente HTTP que realiza las peticiones al servidor del módulo de tarjetas.
+     */
+
+    constructor(apiClient){
+        super();
+        this.apiClient = apiClient
+    }
+
+    /**
+     * Obtiene el saldo de un cliente.
+     *
+     * @async
+     * @param {string} userId - Id del usuario.
+     * @returns {Promise<Client>} Entidad `Client` con la información recuperada.
+     * @throws {Error} Si la respuesta no contiene un cliente válido.
+     */
+    async getCardBalance(clientId) {
+        const response = await this.apiClient.getCardBalance(clientId);
+
+        //Para acceder al body
+        const data = response.data;
+
+        return new Card({
+            cardId: data.id_tarjeta,
+            clientId: data.id_cliente,
+            levelId: data.id_nivel,
+            balance: data.saldo,
+        });
+    }
+}
