6e7c508389579bd58ac2e653dc49921c66f9940f
Morgana119 <145613786+Morgana119@users.noreply.github.com>
2026-05-18 20:32:33 -0600

    [AGREGAR] Comentarios en componentes

--- src/components/atoms/Counters.jsx
+++ src/components/atoms/Counters.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import "../../css/atoms/counters.css";
 
+/**
+ * Componente de contador reutilizable para mostrar métricas o estadísticas clave.
+ * 
+ * @param {string} [label=""] - Etiqueta descriptiva del contador.
+ * @param {number} [value=0] - Valor numérico a mostrar en el contador.
+ * @return {JSX.Element} Componente de contador con etiqueta y valor.
+ */
 export default function Counters({ 
     label = '',
     value = 0,
--- src/components/molecules/CountersGroup.jsx
+++ src/components/molecules/CountersGroup.jsx
@@ -3,6 +3,14 @@ import "../../css/atoms/counters.css";
 import "../../css/molecules/countersGroup.css";
 import Counters from '../atoms/Counters';
 
+/**
+ * Componente que agrupa múltiples contadores para mostrar varias métricas o 
+ * estadísticas relacionadas.
+ * 
+ * @param {Array<{label: string, value: number}>} [counters=[]] - Lista de objetos
+ * con etiqueta y valor para cada contador.
+ * @return {JSX.Element} Componente que renderiza un grupo de contadores.
+ */
 export default function CountersGroup({ counters = [] }) {
     return (
         <div className="counters-group">
