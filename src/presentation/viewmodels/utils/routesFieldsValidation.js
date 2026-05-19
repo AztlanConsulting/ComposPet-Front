1334b7fb72803ec4def77227f64226d4eaeeb8e9
JuanManuelMurilloLopez <142356988+JuanManuelMurilloLopez@users.noreply.github.com>
2026-05-19 17:21:08 -0600

    [CORREGIR] Validaciones sin utilizar

--- src/presentation/viewmodels/utils/routesFieldsValidation.js
+++ src/presentation/viewmodels/utils/routesFieldsValidation.js
@@ -56,46 +56,6 @@ export const validateSchedule = (value) => {
     return true;
 }
 
-export const validatePhone = (value) => {
-    if (!value) return "El teléfono es requerido.";
-
-    const phoneRegex = /^(\+52[\s-]?)?[0-9]{3}[\s-]?[0-9]{3}[\s-]?[0-9]{4}$/;
-
-    if (!phoneRegex.test(value)) return "Ingresa un teléfono válido.";
-
-    return true;
-};
-
-export const validateAddress = (value) => {
-    if (!value) return "La dirección es requerida.";
-
-    const addressRegex = /^[a-zA-ZÀ-ÿ0-9\s.,#-]{5,150}$/;
-
-    if (!addressRegex.test(value)) return "Ingresa una dirección válida";
-
-    return true;
-};
-
-export const validateText = (value, fieldName) => {
-    if (!value) return ` Campo ${fieldName} es requerido.`;
-
-    if (value.length > 100) return `${fieldName} demasiado largo.`;
-
-    return true;
-};
-
-export const validateOrder = (value) => {
-    if (!value) return "El orden es requerido.";
-
-    if(value < 0) return "El orden no puede ser negativo.";
-
-    if(value == 0) return "El orden no puede ser 0.";
-
-    if(value >= 100) return "El orden es demasiado grande.";
-
-    return true;
-}
-
 export const validateField = (field, value) => {
 
     switch (field) {
@@ -113,21 +73,6 @@ export const validateField = (field, value) => {
 
         case "paid":
             return validatePaid(value);
-
-        case "cellphone":
-            return validatePhone(value);
-
-        case "address":
-            return validateAddress(value);
-
-        case "pets":
-            return validateText(value, "Mascotas");
-
-        case "family":
-            return validateText(value, "Familia");
-
-        case "order":
-            return validateOrder(value);
 
         default:
             return true;
