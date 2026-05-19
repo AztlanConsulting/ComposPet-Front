db41fc70b976c52ff28576feddf21497fb14ef03
Morgana119 <145613786+Morgana119@users.noreply.github.com>
2026-05-19 16:04:46 -0600

    [AGREGAR] Contadores de saldo en rutas

--- src/components/organisms/BalanceCountersGroup.jsx
+++ src/components/organisms/BalanceCountersGroup.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import BalanceCounter from '../molecules/BalanceCounter';
-
 import '../../css/organisms/balanceCountersGroup.css';
 
 /**
--- src/presentation/views/routesInfo/routesInfo.jsx
+++ src/presentation/views/routesInfo/routesInfo.jsx
@@ -3,8 +3,7 @@ import Navbar from "../../../components/molecules/Navbar";
 import CopyLink from "../../../components/molecules/CopyLink";
 import useRoutesViewModel from "../../viewmodels/routesInfo/routesTable";
 import ColorsInfo from "./colorsInfo";
-
-import '../../../css/routesInfo/routesInfo.css';
+import BalanceCountersGroup from '../../../components/organisms/BalanceCountersGroup';
 
 export default function RoutesInfo(){
     const { copyLinkInfo } = useRoutesViewModel();
@@ -12,8 +11,29 @@ export default function RoutesInfo(){
     return(
         <div className="page">
             <Navbar />
-            <div className="copy-link-container">
-                <CopyLink {...copyLinkInfo} />
+            <div className="route-header-row">
+                <BalanceCountersGroup
+                    routeCounter={{
+                        title: 'Saldo total de ruta',
+                        favorSubtitle: 'Saldo a favor',
+                        favorBalance: '$361',
+
+                        pendingSubtitle: 'Saldo pendiente',
+                        pendingBalance: '- $147',
+                    }}
+
+                    totalCounter={{
+                        title: 'Saldo total semanal',
+                        favorSubtitle: 'Saldo a favor',
+                        favorBalance: '$361',
+
+                        pendingSubtitle: 'Saldo pendiente',
+                        pendingBalance: '- $147',
+                    }}
+                />
+                <div className="copy-link-container">
+                    <CopyLink {...copyLinkInfo} />
+                </div>
             </div>
             <RoutesTablePage />
             {/* Agrega información de colores */}
