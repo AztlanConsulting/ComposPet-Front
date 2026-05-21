bea6e3d3b2118526e780c50b81a42cc7d12fbf3e
Morgana119 <145613786+Morgana119@users.noreply.github.com>
2026-05-21 15:32:18 -0600

    [CORREGIR] css de rutas

--- src/presentation/views/routesInfo/routesInfo.jsx
+++ src/presentation/views/routesInfo/routesInfo.jsx
@@ -5,6 +5,7 @@ import useRoutesViewModel from "../../viewmodels/routesInfo/routesTable";
 import ColorsInfo from "./colorsInfo";
 import BalanceCountersGroup from '../../../components/organisms/BalanceCountersGroup';
 import CountersGroup from '../../../components/molecules/CountersGroup';
+import '../../../css/routesInfo/routesInfo.css';
 
 export default function RoutesInfo(){
     const routesViewModel = useRoutesViewModel();
@@ -21,36 +22,34 @@ export default function RoutesInfo(){
     return(
         <div className="page">
             <Navbar />
+            <div className="main">
+                <div className="route-header-row">
+                    <CountersGroup 
+                        counters={[
+                            { label: "Sumatoria total", value: dayTotalAmount },
+                        ]}
+                    />
 
-            <div className="route-header-row">
-                <CountersGroup 
-                    counters={[
-                        { label: "Sumatoria total", value: dayTotalAmount },
-                    ]}
-                />
-
-                <BalanceCountersGroup
-                    counters={[
-                        {
-                            title: 'Ruta',
-                            favorSubtitle: 'Pagado',
-                            favorBalance: routePayedAmount,
-                            pendingSubtitle: 'Pendiente',
-                            pendingBalance: routePendingAmount,
-                        },
-                        {
-                            title: 'Semana',
-                            favorSubtitle: 'Pagado',
-                            favorBalance: weeklyPayedAmount,
-                            pendingSubtitle: 'Pendiente',
-                            pendingBalance: weeklyPendingAmount,
-                        },
-                    ]}
-                />
-
-                <div className="copy-link-container">
-                    <CopyLink {...copyLinkInfo} />
+                    <BalanceCountersGroup
+                        counters={[
+                            {
+                                title: 'Ruta',
+                                favorSubtitle: 'Pagado',
+                                favorBalance: routePayedAmount,
+                                pendingSubtitle: 'Pendiente',
+                                pendingBalance: routePendingAmount,
+                            },
+                            {
+                                title: 'Semana',
+                                favorSubtitle: 'Pagado',
+                                favorBalance: weeklyPayedAmount,
+                                pendingSubtitle: 'Pendiente',
+                                pendingBalance: weeklyPendingAmount,
+                            },
+                        ]}
+                    />
                 </div>
+                    <CopyLink {...copyLinkInfo} />
             </div>
 
             <RoutesTablePage routesViewModel={routesViewModel} />
