e4ff71437a0aae2f24cb118574619ff56bca947f
Ale Arredondo <alejandrarredondo09@gmail.com>
2026-05-25 17:57:00 -0600

    [MODIFICAR] detalles de interfaz

--- src/components/molecules/Navbar.jsx
+++ src/components/molecules/Navbar.jsx
@@ -100,10 +100,16 @@ export default function Navbar() {
 
             {/* Cerrar sesión — solo desktop */}
             <div className="navbarRight">
-                {user != null && (
+                {user != null ? (
                     <NavbarItem route="/" logout={true} onClick={handleLogout}>
                         Cerrar sesión
                     </NavbarItem>
+                ) : (
+                    <div className="login">
+                        <NavbarItem route="/inicio-sesion">
+                            Iniciar sesión
+                        </NavbarItem>
+                    </div>
                 )}
             </div>
 
