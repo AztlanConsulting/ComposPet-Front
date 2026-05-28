375cdf7412e8b8d5befe721379a72abcaeddb9cd
Yessica <a01273574@tec.mx>
2026-05-28 09:58:21 -0600

    [AGREGAR] Librería Performance

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './utilities/reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import 'bootstrap/dist/css/bootstrap.min.css';  
 
@@ -31,4 +31,20 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*reportWebVitals((metric) => {
+  const metricData = {
+    metric: metric.name,
+    value: Number(metric.value.toFixed(2)),
+    rating: metric.rating || 'No disponible',
+    delta: Number(metric.delta.toFixed(2)),
+    id: metric.id,
+  };
+
+  console.log(`[Web Vital] ${metric.name}:`, metricData);
+});*/
+
+reportWebVitals((metric) => {
+  console.log(
+    `[Web Vital] ${metric.name} | Valor: ${metric.value.toFixed(2)} ms | Delta: ${metric.delta.toFixed(2)}`
+  );
+});
