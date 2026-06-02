2ed3ecb86070f6275e706922e086f7ba5134b9af
JuanManuelMurilloLopez <a01712218@tec.mx>
2026-06-01 21:03:34 -0600

    [MODIFICAR] clase de columna de estatus de cliente para que no sea confuso si se puede editar o no

--- src/presentation/viewmodels/utils/routesFieldsValidation.js
+++ src/presentation/viewmodels/utils/routesFieldsValidation.js
@@ -10,7 +10,9 @@ export const validateCollected = (value) => {
 
     if (isNaN(value)) return "El número de cubetas debe de ser un número.";
 
-    if(value > 100) return "Las cubetas recolectadas no pueden ser más de 100.";
+    if (value < 0) return "El número de cubetas debe de ser positivo.";
+
+    if(value > 20) return "Las cubetas recolectadas no pueden ser más de 20.";
 
     if(isNaN(value)) return "El número de cubetas debe de ser un número.";
 
