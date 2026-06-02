4bc06ce22486ad5dd0e39cf286024f466557c2c8
JuanManuelMurilloLopez <a01712218@tec.mx>
2026-06-01 23:33:15 -0600

    [MODIFICAR] tipo de entrada de campos de email en login para mandar mensaje de retroalimentación personalizado

--- src/presentation/viewmodels/auth/loginViewModel.js
+++ src/presentation/viewmodels/auth/loginViewModel.js
@@ -131,6 +131,7 @@ function useLoginViewModel(loginUseCase){
         e.preventDefault();
 
         const { errors: validationErrors, hasErrors } = validateLoginForm(email, password); 
+        console.log(errors);
 
         if (hasErrors){
             setErrors(validationErrors);
